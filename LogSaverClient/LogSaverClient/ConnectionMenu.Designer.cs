1e8940ee5264d3e835afb3616c90405b5b3c0cde
Dylan-Dotti <invocation42@yahoo.com>
2020-10-21 20:41:28 -0400

    Changes to ConnectionMenu form

--- LogSaverClient/LogSaverClient/ConnectionMenu.Designer.cs
+++ LogSaverClient/LogSaverClient/ConnectionMenu.Designer.cs
@@ -31,6 +31,8 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.connectButton = new System.Windows.Forms.Button();
             this.ipInput = new System.Windows.Forms.TextBox();
+            this.enterIPLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,25 +43,30 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
             this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.connectButton, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.ipInput, 0, 0);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.resultLabel, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.connectButton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ipInput, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.enterIPLabel, 0, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(576, 250);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(864, 385);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // connectButton
             // 
             this.connectButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.connectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.connectButton.Location = new System.Drawing.Point(163, 128);
+            this.connectButton.Location = new System.Drawing.Point(244, 190);
+            this.connectButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.connectButton.Name = "connectButton";
-            this.connectButton.Size = new System.Drawing.Size(250, 40);
+            this.connectButton.Size = new System.Drawing.Size(375, 62);
             this.connectButton.TabIndex = 1;
             this.connectButton.Text = "Connect";
             this.connectButton.UseVisualStyleBackColor = true;
@@ -69,19 +76,44 @@
             // 
             this.ipInput.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.ipInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ipInput.Location = new System.Drawing.Point(163, 87);
+            this.ipInput.Location = new System.Drawing.Point(245, 132);
+            this.ipInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ipInput.Name = "ipInput";
-            this.ipInput.Size = new System.Drawing.Size(250, 35);
+            this.ipInput.Size = new System.Drawing.Size(373, 48);
             this.ipInput.TabIndex = 0;
             this.ipInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // enterIPLabel
+            // 
+            this.enterIPLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.enterIPLabel.AutoSize = true;
+            this.enterIPLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterIPLabel.Location = new System.Drawing.Point(354, 90);
+            this.enterIPLabel.Name = "enterIPLabel";
+            this.enterIPLabel.Size = new System.Drawing.Size(156, 37);
+            this.enterIPLabel.TabIndex = 2;
+            this.enterIPLabel.Text = "Server IP:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(377, 272);
+            this.resultLabel.Margin = new System.Windows.Forms.Padding(3, 15, 3, 0);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(109, 26);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.Text = "Server IP:";
+            // 
             // ConnectionMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ConnectionMenu";
-            this.Size = new System.Drawing.Size(576, 250);
+            this.Size = new System.Drawing.Size(864, 385);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
@@ -93,5 +125,7 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button connectButton;
         private System.Windows.Forms.TextBox ipInput;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label enterIPLabel;
     }
 }
