e603aadd99141832a1d92b6b0764c93442708bcc
Dylan Dotti <invocation42@yahoo.com>
2020-11-10 18:24:22 -0500

    Added BinaryTcpClient and ClientRequestHandler

---
+++ LogSaverServer/LogSaverServer/BinaryTcpClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace LogSaverServer
+{
+    class BinaryTcpClient
+    {
+        public TcpClient Client { get; private set; }
+        public BinaryReader Reader { get; private set; }
+        public BinaryWriter Writer { get; private set; }
+
+        public BinaryTcpClient(TcpClient client)
+        {
+            Client = client;
+            Reader = new BinaryReader(client.GetStream());
+            Writer = new BinaryWriter(client.GetStream());
+        }
+    }
+}
---
+++ LogSaverServer/LogSaverServer/ClientRequestHandler.cs
@@ -0,0 +1,55 @@
+﻿using Messages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LogSaverServer
+{
+    class ClientRequestHandler
+    {
+        private BinaryTcpClient client;
+        private FileOperator foperator;
+
+        private readonly string logsSourcePath;
+        private readonly string logsDestPath;
+
+        public ClientRequestHandler(BinaryTcpClient client,
+            string logsSourcePath, string logsDestPath)
+        {
+            this.client = client;
+            foperator = new FileOperator();
+        }
+
+        public void HandleZipRequest(ZipRequestMessage request)
+        {
+            string zipPath = Path.Combine(logsDestPath, request.ZipFileName);
+            if (File.Exists(zipPath))
+            {
+                SendResponseMessage(ResponseCode.Error,
+                    $"Zip archive with the name {request.ZipFileName} already exists.");
+            }
+            else
+            {
+                // send response
+                SendResponseMessage(ResponseCode.Ok);
+                // zip directory
+                string[] filePaths = foperator.GetFilteredFilePaths(logsSourcePath,
+                    request.TimeRangeLocal, request.FullCategories);
+                foperator.ZipFiles(filePaths, zipPath, client.Writer);
+            }
+        }
+
+        public void HandleTransferRequest()
+        {
+
+        }
+
+        private void SendResponseMessage(ResponseCode resCode, string message = "")
+        {
+            var response = new ResponseMessage(resCode, message);
+            client.Writer.Write(response);
+            FileLogger.Log("Sent response: " + response.ToString(true));
+        }
+    }
+}
